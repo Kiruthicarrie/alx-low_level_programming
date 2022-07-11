0155ed6bb38f557c0b40efac93dad50aca276c35
Kiruthcarrie <carolinewangari74@gmail.com>
2022-07-11 12:17:23 -0700

    Task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,12 @@ int main(void)
 int i;
 
 for (i = 1; i <= 100 ; i++)
-
 {
-
 if (i % 3 == 0 && i % 5 == 0)
-
 printf("FizzBuzz ");
-
 else if (i % 3 == 0)
-
 {
-
 printf("Fizz ");
-
 }
 
 else if (i % 5 == 0 && i != 100)
@@ -46,13 +39,10 @@ printf("Buzz");
 else
 
 printf("%d ", i);
-
 }
 
 }
-
 printf("\n");
 
 return (0);
-
 }
