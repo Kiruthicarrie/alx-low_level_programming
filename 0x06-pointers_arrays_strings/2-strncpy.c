1d2a8e96b0a2a2983d916746b435e0dedf793162
Kiruthcarrie <carolinewangari74@gmail.com>
2022-07-14 12:40:24 -0700

    Task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,21 +2,16 @@
 
 /**
  * _strncat - concatenates n bytes from a string to another
- *
  * @dest: destination string
  * @src: source string
  * @n: number of bytes of str to concatenate
- *
  * Return: a pointer to the resulting string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int x = 0;
-
 	int y = 0;
-
-
 
 	while (dest[x])
 
@@ -29,13 +24,11 @@ char *_strncat(char *dest, char *src, int n)
 	while (y < n && src[y] != 0)
 
 	{
-
 		dest[x] = src[y];
 
 		x++;
 
 		y++;
-
 	}
 
 	dest[x] = 0;
