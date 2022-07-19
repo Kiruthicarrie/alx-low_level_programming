7ee15d6200cdbe2cfeece4095802dbd87629336a
Kiruthcarrie <carolinewangari74@gmail.com>
2022-07-19 02:03:56 -0700

    Task0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+char *_memset(char *s, char b, unsigned int n);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strpbrk(char *s, char *accept);
+
+char *_strstr(char *haystack, char *needle);
+
+char *_strpbrk(char *s, char *accept);
+
+void print_chessboard(char (*a)[8]);
+
+void print_diagsums(int *a, int size);
+
+
+
+#endif
