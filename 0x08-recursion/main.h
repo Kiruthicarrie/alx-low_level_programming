837320025790febf5c3a0c5ca9cccac2e62db04a
Kiruthcarrie <carolinewangari74@gmail.com>
2022-07-21 13:21:14 -0700

    Task0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+int primenumber(int n, int m);
+
+int is_prime_number(int n);
+
+void _puts_recursion(char *s);
+
+int _putchar(char c);
+
+void _print_rev_recursion(char *s);
+
+int sqtChecker(int min, int max, int m);
+
+
+
+#endif
