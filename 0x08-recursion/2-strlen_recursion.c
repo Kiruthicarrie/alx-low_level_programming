9d38ed09ecdfb6e00a239df2be2b85c282e74d29
Kiruthcarrie <carolinewangari74@gmail.com>
2022-07-21 13:27:12 -0700

    Task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - Main funtion
+ * @s: Pointer to String
+ * Return: Void
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s != '\0')
+
+
+		return (_strlen_recursion(s + 1) + 1);
+
+
+
+	else
+
+
+
+		return (0);
+
+}
