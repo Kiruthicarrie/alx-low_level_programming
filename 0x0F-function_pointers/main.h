3e3e6e963969ff50ed2b381fcd223e0a302a76ac
Kiruthcarrie <carolinewangari74@gmail.com>
2022-08-03 16:21:19 -0700

    pointers

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,9 +1,30 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+
+#include <string.h>
 #include <stddef.h>
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
 #endif
