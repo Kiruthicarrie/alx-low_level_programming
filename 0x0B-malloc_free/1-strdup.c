582c7210f837e94b17dc4e0329d60a364217ccc7
Kiruthcarrie <carolinewangari74@gmail.com>
2022-08-03 15:11:59 -0700

    Malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: char
+ * Description:  contains a copy of the string given as a parameter.
+ * Return: pointer to char
+ */
+char *_strdup(char *str)
+{
+char *ptrStr;
+int  i;
+
+if (str ==  NULL)
+return (NULL);
+
+for (i = 0; str[i] != '\0' ; i++)
+;
+
+ptrStr =  malloc((i + 1) * sizeof(char));
+
+if (ptrStr == NULL)
+return (NULL);
+
+for (i = 0; str[i] != '\0' ; i++)
+{
+ptrStr[i] = str[i];
+}
+
+ptrStr[i] = '\0';
+
+return (ptrStr);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: ancho
+ * @height: alto
+ * Description:  returns a pointer to a 2 dimensional array of integers.
+ * Return: pointer to char
+ */
+int **alloc_grid(int width, int height)
+{
+int **ptrMatriz;
+int ancho, alto;
+
+if ((width < 1) || (height < 1))
+return (NULL);
+
+ptrMatriz = malloc(height * sizeof(int *));
+if (ptrMatriz == NULL)
+{
+free(ptrMatriz);
+return (NULL);
+}
+
+for (alto = 0 ; alto <  height ; alto++)
+{
+ptrMatriz[alto] = malloc(width * sizeof(int));
+if (ptrMatriz[alto] == NULL)
+{
+for ( ; alto >= 0 ; alto--)
+free(ptrMatriz[alto]);
+free(ptrMatriz);
+return (NULL);
+}
+}
+for (alto = 0 ; alto < height ; alto++)
+{
+for (ancho = 0 ; ancho < width ; ancho++)
+ptrMatriz[alto][ancho] = 0;
+}
+return (ptrMatriz);
+}
