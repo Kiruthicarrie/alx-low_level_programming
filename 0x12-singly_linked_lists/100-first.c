383cd6f1c14e9af792874137c452f867129ad6c8
Kiruthcarrie <carolinewangari74@gmail.com>
2022-08-17 06:36:36 -0700

    Singly linked Lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * __start - execute before main
+ * Description: execute before main
+ * Return: Nothing
+ */
+#include <stdio.h>
+/**
+ * __start - execute before main
+ * Description: execute before main
+ * Return: Nothing
+ */
+void __start(void) __attribute__((constructor));
+
+void __start(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
